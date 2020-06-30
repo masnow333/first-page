f31dc0307d16e960276ce86b91e7762683954c4a
Jonathan Triana <jonfertri357@gmail.com>
2020-06-29 20:48:16 -0500

    The function in js/slider.js was modificated by a class

--- js/slider.js
+++ js/slider.js
@@ -1,27 +1,33 @@
 /* slider */
 
-let slides = document.querySelectorAll('.sliderContainer > .slides');
+class slider{
+    constructor(){
+        let slides = document.querySelectorAll('.sliderContainer > .slides');
+        setInterval(() => this.transition(slides), 5000);
+        
+    }
 
-setInterval(slider, 5000);
-
-function slider() {
-    for (let i = 0; i < slides.length; i++) {
-        if (i == slides.length -1) {
-            for (let index = 0; index < slides.length; index++) {
-                slides[index].classList.add("opacity");
-            }
-            slides[0].classList.remove("opacity");
-            break;
-        }else{
-            if (!   slides[i].classList.contains("opacity")) {
+    transition(slides){
+        for (let i = 0; i < slides.length; i++) {
+            if (i == slides.length -1) {
                 for (let index = 0; index < slides.length; index++) {
                     slides[index].classList.add("opacity");
                 }
-                slides[i +1].classList.remove("opacity");
+                slides[0].classList.remove("opacity");
                 break;
+            }else{
+                if (!   slides[i].classList.contains("opacity")) {
+                    for (let index = 0; index < slides.length; index++) {
+                        slides[index].classList.add("opacity");
+                    }
+                    slides[i +1].classList.remove("opacity");
+                    break;
+                }
             }
         }
     }
 }
+
+const Slider = new slider();
 
 /* end slider */
